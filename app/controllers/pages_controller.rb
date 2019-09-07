e8857eec23b6fd7446e3e7ae99f6211ba4112a8d
Alisonhardison <appletini19@gmail.com>
2019-09-07 16:06:42 +0000

    added first idea to index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,16 @@
 class PagesController < ApplicationController
 
   def index
-  @pages = Page.all
+    @pages = Page.all
+    @ideas = Idea.all
   end
 
   def new
-  @page = Page.new
+    @page = Page.new
   end
 
   def info
-  redirect_to new_page  
+    redirect_to new_page  
   end
 
 end
